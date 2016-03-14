db466b4a7fbfe607208e59d62d8d243a4dc71e90
Luca Pistolesi <luke.pistolesi@gmail.com>
2016-03-14 22:00:35 +1100

    First commit with minimal setup

---
+++ test/MainTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Main.class)
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
